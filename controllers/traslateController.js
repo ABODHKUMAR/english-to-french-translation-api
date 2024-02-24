9020fed0e3c7185981a92049ba2c0423f1e8cf05
abodh5921 <abodh5921@gmail.com>
2024-02-24 12:46:47 +0530

    Add NEW API KEY

--- controllers/traslateController.js
+++ controllers/traslateController.js
@@ -8,30 +8,38 @@ const translateController = async (req, res) => {
             return res.status(400).json({ error: 'Text to translate is required' });
         }
 
-        const encodedParams = new URLSearchParams();
-        encodedParams.set('from', 'en');
-        encodedParams.set('to', 'fr');
-        encodedParams.set('text', text); // Corrected variable name to 'text'
-
         const options = {
-          method: 'POST',
-          url: 'https://google-translate113.p.rapidapi.com/api/v1/translator/text',
-          headers: {
-            'content-type': 'application/x-www-form-urlencoded',
-            'X-RapidAPI-Key': '3926835e04msh1bd925d479a3f19p16ecd4jsn71da831a39ad',
-            'X-RapidAPI-Host': 'google-translate113.p.rapidapi.com'
-          },
-          data: encodedParams,
+            method: 'POST',
+            url: 'https://deep-translate1.p.rapidapi.com/language/translate/v2',
+            headers: {
+                'content-type': 'application/json',
+                'X-RapidAPI-Key': '3926835e04msh1bd925d479a3f19p16ecd4jsn71da831a39ad',
+                'X-RapidAPI-Host': 'deep-translate1.p.rapidapi.com'
+            },
+            data: {
+                q: text,
+                source: 'en',
+                target: 'fr'
+            }
         };
 
         try {
             const response = await axios.request(options);
-            console.log(response);
+            // Check if response data contains translation
+            if (response.data && response.data.data && response.data.data.translations && response.data.data.translations.translatedText) {
+                const translation = response.data.data.translations.translatedText;
+                return res.json({ translation: translation });
+            } else {
+                return res.status(500).json({ error: 'Translation not found' });
+            }
         } catch (error) {
-            console.error('Error in translating text:', error);
+            // Handle axios request error
+            console.error('Error during translation:', error);
             return res.status(500).json({ error: 'Translation service error' });
         }
+
     } catch (error) {
+        // Handle other errors
         console.error('Error in handling translation request:', error);
         return res.status(500).json({ error: 'Internal server error' });
     }
